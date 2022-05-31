aa4ad7f6e4f92afc7e4f3069885d92907c2b4e66
Metz01 <camillomatteo01@gmail.com>
2022-05-31 19:22:56 +0200

    - Cleaner Code and english version

--- CaesarCipher.c
+++ CaesarCipher.c
@@ -1,52 +1,20 @@
 #include<stdio.h>
 
-void Crypt(char *text, int key);
-void Clear(char *text);
 void PrintMainMenu(int cryptographyKey);
+int SelectMenuOption();
+int GetUserMenuItem(int invalidChoose);
+int UserActionHandler(int userMenuChoose, int *cryptographyKey, int maxMessageSize);
+int GetCryptographyKey();
+int CheckValidKey(int cryptographyKey);
+int GetMessage(char *message, int cryptographyKey, int maxMessageSize);
+void CryptMessage(char *text, int key);
 
 int main(){
-    //choose -> choose of the user in the menu
-    //pick -> check variable to see if is the first loop of the while
-    int maxStringSize = 100, cryptographyKey = 0, choose, pick;
-    char text[maxStringSize];
+    int maxStringSize = 100, cryptographyKey = 0, userMenuChoose;
     while(1==1){
-        choose = 0, pick = 0;
         PrintMainMenu(cryptographyKey);
-        
-        do{
-            //if is the first time (pick == 0) you enter there don't print "not valid choise"
-            printf("%sScegliere voce del menù e premere invio: ", pick == 0 ? "" : "Scelta non presente nel menù.\n");
-            //take in input the choose of the user and check if is valid or it generates a system error
-            if(scanf("%d", &choose) != 1){
-                printf("Input non valido\n");
-                return -1;
-            }
-            pick ++;
-        }while(choose < 1 || choose > 4); //repate until the choose is in the valid range
-        pick = 0;
-        if(choose == 1){
-            do{
-                printf("%sInserire la chiave di crittografia: ", pick == 0 ? "" : "Chiave inserita non valida.\n");
-                //take in input the cryptography key of the user and check if is valid or it generates a system error
-                if(scanf("%d", &cryptographyKey) != 1){
-                    printf("Input non valido\n");
-                    return -1;
-                }
-                pick++;
-            }while (cryptographyKey == 0);
-        }else if(choose == 2 || choose == 3){
-            if(cryptographyKey == 0){
-                printf("Prima di continuare impostare una chiave valida\n");
-                continue;
-            }
-            printf("\n(lunghezza massima mex 100 caratteri)\nInserire il messaggio: ");
-            Clear(text);
-            scanf("%*c%[^\n]%*c", text);
-            //Call the Crypt method whit key if is to crypt the message else -key if it is to decrypt it
-            //Decrypting is the same process of Crypting but with -(key)
-            Crypt(text, choose == 2 ? cryptographyKey : -cryptographyKey);
-            printf("%srypt: %s\n\n", choose == 2 ? "C" : "Dec", text);
-        }else if(choose == 4) break;
+        userMenuChoose = SelectMenuOption();
+        if(UserActionHandler(userMenuChoose, &cryptographyKey, maxStringSize) < 0) break;
     }      
 }
 
@@ -60,8 +28,68 @@ void PrintMainMenu(int cryptographyKey){
     printf("4) Quit\n\n");
 }
 
-//calculate the length of a string (how many chars)
-int size(char* a){
+int SelectMenuOption(){
+    int userMenuChoose, invalidChoose = 0;
+    do{
+        userMenuChoose =  GetUserMenuItem(invalidChoose);
+        invalidChoose ++;
+    }while(userMenuChoose < 1 || userMenuChoose > 4); //repate until the choose is in the valid range
+    return userMenuChoose;
+}
+
+int GetUserMenuItem(int invalidChoose){
+    int choose;
+    printf("%s", invalidChoose == 0 ? "" : "Not valid choose.\n");
+    printf("Select an item from the menu: ");
+    if(scanf("%d", &choose) != 1){
+        printf("Not valid Input\n");
+        return -1;
+    }
+    return choose;
+}
+
+int UserActionHandler(int userMenuChoose, int *cryptographyKey, int maxMessageSize){
+    if (userMenuChoose == 1){
+        *cryptographyKey = GetCryptographyKey();
+            return 1;
+    }else if (userMenuChoose == 2 || userMenuChoose == 3){
+        char message[maxMessageSize];
+        if(GetMessage(message, *cryptographyKey, maxMessageSize) < 0) return 1;
+        CryptMessage(message, userMenuChoose == 2 ? *cryptographyKey : -(*cryptographyKey));
+        printf("%srypt: %s\n\n", userMenuChoose == 2 ? "C" : "Dec", message);
+        return 1;
+    }else return -1;
+}
+
+int GetCryptographyKey(){
+    int cryptographyKey;
+    printf("Insert cryptography key: ");
+    if(scanf("%d", &cryptographyKey) != 1){
+        printf("Not a valid input\n");
+        return -1;
+    }
+    if(CheckValidKey(cryptographyKey) == -1) return GetCryptographyKey();
+    return cryptographyKey;
+}
+
+int CheckValidKey(int cryptographyKey){
+    if(cryptographyKey == 0){
+        printf("Invalid Key inserted \n");
+        return -1;
+    }else return cryptographyKey;
+}
+
+int GetMessage(char *message, int cryptographyKey, int maxMessageSize){
+    if(cryptographyKey == 0){
+        printf("Before continuing insert a valid key\n");
+        return -1;
+    }
+    printf("\n(maximum message length is %d char)\nInsert message: ", maxMessageSize);
+    scanf("%*c%[^\n]%*c", message);
+    return 1;
+}
+
+int StringSize(char* a){
     int l = 0;
     while(*(a+l) != '\0'){
         l++;
@@ -74,17 +102,12 @@ int mod(int n, int mod){
     return (n >= 0 ? n : (mod+(n%mod)))%mod;
 }
 
-void Crypt(char *text, int key){
-    int l = size(text), n;
+void CryptMessage(char *text, int key){
+    int l = StringSize(text), n;
     //transform char by char of the string
     for(int i = 0; i < l; i++){
         if(*(text+i) <= '9' && *(text+i) >= '0') *(text+i) = (mod(*(text+i)-'0'+key,10)+'0');
         else if(*(text+i) <= 'z' && *(text+i) >= 'a') *(text+i) = (mod(*(text+i)-'a'+key,26)+'a');
         else if(*(text+i) <= 'Z' && *(text+i) >= 'A') *(text+i) = (mod(*(text+i)-'A'+key,26)+'A');
     }
-}
-
-//put a '\0' (end of string) at the start of a char array, like so the string is interpreted as empty 
-void Clear(char *text){
-    *(text) = '\0';
 }
